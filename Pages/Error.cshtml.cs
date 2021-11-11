a85f40e96cde0a79ed583e45e4b1c6bdc4b56660
nathan-schubkegel <nathan.schubkegel@gmail.com>
2021-11-10 23:41:21 -0800

    Initial addition of template razor pages

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -7,21 +7,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace CryptoProfiteer.Pages
+namespace dotnetpoo.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
         private readonly ILogger<ErrorModel> _logger;
 
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
-
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         public void OnGet()
         {
--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace CryptoProfiteer
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddRazorPages();
       services.AddControllersWithViews();
 
       services.AddSingleton<PersistenceService>();
@@ -34,10 +35,24 @@ namespace CryptoProfiteer
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseStaticFiles();
+
       app.UseRouting();
 
       app.UseEndpoints(endpoints =>
       {
+        endpoints.MapRazorPages();
         endpoints.MapControllerRoute(
           name: "default",
           pattern: "{controller=TheController}/{action=Index}/{id?}");
