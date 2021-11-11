ea6f2e418d55ba10b482b06b7805169a9cec826b
nathan-schubkegel <nathan.schubkegel@gmail.com>
2021-11-10 23:41:57 -0800

    Personalize the razor pages. Add Transactions page.

--- Pages/Transactions.cshtml.cs
+++ Pages/Transactions.cshtml.cs
@@ -5,21 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using CryptoProfiteer.Services;
 
 namespace CryptoProfiteer.Pages
 {
-  public class IndexModel : PageModel
+  public class TransactionsModel : PageModel
   {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<TransactionsModel> _logger;
+    private readonly IPersistenceService _p;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public TransactionsModel(ILogger<TransactionsModel> logger, IPersistenceService p)
     {
       _logger = logger;
+      _p = p;
     }
+
+    public PersistenceData Data => _p.Data;
 
     public void OnGet()
     {
-
     }
   }
 }
