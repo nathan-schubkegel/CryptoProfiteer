3c274c2b5bc828ad752802d22c835c9494fc5341
nathan-schubkegel <nathan.schubkegel@gmail.com>
2023-04-15 11:37:59 -0700

    Make the FreeTaxUSA script work on my other PC

--- TheController.cs
+++ TheController.cs
@@ -1076,51 +1076,51 @@ else
 
 ; ""it's a crypto"" button
 Click, 847 483
-Sleep, 1500
+Sleep, 500
 
 ; ""save and continue"" 
 Click, 1156 666
-Sleep, 2500
+Sleep, 1500
 
 ; ""both"" (Nathan and Rachel) 
 Click, 438 517
-Sleep, 1500
+Sleep, 500
 
 ; ""save and continue"" 
 Click, 1171 609
-Sleep, 2500
+Sleep, 1500
 
 ; ""one at a time"" 
 Click, 742 530
-Sleep, 1500
+Sleep, 500
 
 ; ""save and continue""
 Click, 1173 741
-Sleep, 2500
+Sleep, 1500
 
 ; description textbox 
 Click, 900 475
-Sleep, 1500
+Sleep, 500
 Send, {0}
 
 ; date acquired box  (month/day/year)
 Click, 902 663
-Sleep, 1500
+Sleep, 500
 Send, {1}
 
 ; date sold (just month/day)
 Click, 891 744
-Sleep, 1500
+Sleep, 500
 Send, {2}
 
 ; sale proceeds 
 Click, 946 828
-Sleep, 1500
+Sleep, 500
 Send, {3}
 
 ; cost basis
 Click, 938 927
-Sleep, 1500
+Sleep, 500
 Send, {4}
 
 ; pagedown a few times
@@ -1129,24 +1129,39 @@ Send, {{PgDn}}
 Sleep, 1500
 
 ; ""not reported on 1099-B"" 
-Click, 727 605
+Click, 727 726
+Sleep, 500
+
+; ""save and continue"" 
+Click, 1170 1007
 Sleep, 1500
 
 ; ""save and continue"" 
-Click, 1170 885
-Sleep, 2500
-
-; ""save and continue"" 
-Click, 1180 634
+if (({3} = 0) and ({4} = 0))
+{{
+  Click, 1180 957
+}}
+else if ({3} = 0)
+{{
+  MsgBox, Where is it?
+}}
+else if ({4} = 0)
+{{
+  Click, 1180 759
+}}
+else
+{{
+  Click, 1180 634
+}}
 Sleep, 5000
 
 ; press end key
 Send, {{End}}
-Sleep, 3000
+Sleep, 1500
 
 ; ""add another"" button 
-Click, 528 702
-Sleep, 2500
+Click, 528 822
+Sleep, `500
 
       ";
 
@@ -1156,7 +1171,7 @@ Sleep, 2500
       {
         foreach (var purchase in taxAssociation.Purchases.OrderBy(x => x.Order.Time))
         {
-          var description = $"{i++}: {purchase.ContributingCoinCount.FormatMinDecimals()} {taxAssociation.CoinType}";
+          var description = $"{i++}, {purchase.ContributingCoinCount.FormatMinDecimals()} {taxAssociation.CoinType}";
           var dateAcquired = purchase.Order.Time.ToLocalTime().ToString("MM/dd/yyyy");
           var dateSold = taxAssociation.Time.ToLocalTime().ToString("MM/dd");
           
